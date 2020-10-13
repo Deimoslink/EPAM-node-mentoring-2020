38afcad260e78dadf2e323b13d2e95421a8aea04
Aleksandr_Linkov <aleksandr.linkov.consultant@consumer.org>
2020-10-13 15:54:58 +0300

    generateBody method added

--- src/api/auth.controller.ts
+++ src/api/auth.controller.ts
@@ -3,19 +3,24 @@ import {createValidator} from 'express-joi-validation';
 import {UserService} from '../services/users.service';
 import {checkRefreshToken, generateAccessToken, generateRefreshToken} from '../tokens';
 import {authBodySchema} from '../controller-validators/auth.schemas';
+import {UserInstance} from '../types/user.interface';
 
 export const routerAuth: Router = express.Router();
 
 const validator = createValidator();
 const userService: UserService = new UserService();
 
+const generateBody = (user: UserInstance) => {
+    return {
+        "access-token": generateAccessToken({id: user.id, login: user.login}),
+        "refresh-token": generateRefreshToken({id: user.id, login: user.login})
+    }
+};
+
 routerAuth.post('/auth', validator.body(authBodySchema), async (req: Request, res: Response, next: NextFunction) => {
     const user = await userService.findUserByCreds(req.body.login, req.body.password);
     if (user) {
-        res.status(200).json({
-            "access-token": generateAccessToken({id: user.id, login: user.login}),
-            "refresh-token": generateRefreshToken({id: user.id, login: user.login})
-        });
+        res.status(200).json(generateBody(user));
     } else {
         res.status(403).json({status: 'failed', errors: 'Incorrect login or password'});
     }
@@ -23,8 +28,5 @@ routerAuth.post('/auth', validator.body(authBodySchema), async (req: Request, re
 
 routerAuth.post('/refresh-token', checkRefreshToken, async (req: Request, res: Response, next: NextFunction) => {
     const user = res.locals.payload;
-    res.status(200).json({
-        "access-token": generateAccessToken({id: user.id, login: user.login}),
-        "refresh-token": generateRefreshToken({id: user.id, login: user.login})
-    });
+    res.status(200).json(generateBody(user));
 });
